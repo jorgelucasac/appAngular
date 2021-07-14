94b61d6cc897480d7a954a5b380b720d8b9ca769
jorgelucas <jorgelucasl91@gmail.com>
2021-07-14 12:18:58 -0300

    [FEATURE] Customizando a navegação
    - criado o módulo de navegacao
    - criado os componentes de menu, home, footer e not found
    - alterado a index
    - add css para todo o app
    - configurado as rotas inicias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './navegacao/home/home.component';
+import { NotFoundComponent } from './navegacao/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+
+
+
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
