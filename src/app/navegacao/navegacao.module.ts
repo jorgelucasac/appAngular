31867d082bc05d8fab10d7211ac6eae6fb849e66
jorgelucas <jorgelucasl91@gmail.com>
2021-07-14 12:46:06 -0300

    [FEATURE] Configurando o pacote de bootstrap
    - Configurando o collapse menu

--- src/app/navegacao/navegacao.module.ts
+++ src/app/navegacao/navegacao.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
@@ -15,7 +16,10 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+
+    //terceiros
+    NgbModule,
   ],
   exports: [
     FooterComponent,
